d13388db84330cd6c03af46b54f4c76ed0e65b46
Ratopelado <gui.costa366@gmail.com>
2026-08-20 10:50:05 -0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+name: Ci pipeline
+on:
+  push: 
+    branches: [main]
+  pull_request:
+    branches: [main]
+jobs:
+  build-and-test:
+runs-on: ubuntu-latest
+steps:
+  -name:baixar código
+  uses: action/checkout@v4
+
+  -name: Instalar dependências
+  run: npm install
+
+  - name:rodar testes
+  run:npm test
